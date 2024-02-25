1e9f8ba07819ad41aca3d1276a740065073e1136
Ndelevamutua <146925708+Ndelevamutua@users.noreply.github.com>
2024-02-25 14:12:14 +0300

    script.js

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-function showPassword() {
-    var passwordInput = document.getElementById("password");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var introVideo = document.getElementById("introVideo");
+    var loginContainer = document.querySelector(".login-container");
 
-function autoUpperCase(inputField) {
-    inputField.value = inputField.value.toUpperCase();
-}
+    introVideo.addEventListener("ended", function() {
+        introVideo.style.display = "none";
+        loginContainer.style.display = "block";
+    });
 
-function forgotPassword() {
-    var inputPassword = prompt("Enter dndeleva@gmail.com as your email:");
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
 
-    var correctPassword = "dndeleva@gmail.com";
-
-    if (inputPassword === correctPassword) {
-        alert("Thank You for loging In.");
-
-    window.location.href = "main.html";
-    } else {
-    alert("Invalid input. Please try again.");
-    }
-
-}
-
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    if (authenticateUser(email, password)) {
-        alert("Thank You for loging In.");
-
-        window.location.href = "main.html";
-    } else {
-        alert("Invalid Logins. Please try again.");
-    }
+        if (authenticateUser(email, password)) {
+            alert("Thank You for logging In.");
+            window.location.href = "main.html"; // Redirect to main page after successful login
+        } else {
+            alert("Invalid Logins. Please try again.");
+        }
+    });
 });
 
 function authenticateUser(email, password) {
     var validEmail = "dndeleva@gmail.com";
     var validPassword = "Ab5/64257/23";
 
-   return email === validEmail && password === validPassword;
+    return email === validEmail && password === validPassword;
 }
